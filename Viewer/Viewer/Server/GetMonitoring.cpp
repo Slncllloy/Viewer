c9bc4a9075c0a93d5cf7ae0b1ab75c6b38bb68a6
SIncllloy <79789429+SInclloy@users.noreply.github.com>
2022-08-13 23:04:06 +0700

    Update GetMonitoring.cpp

--- Viewer/Viewer/Server/GetMonitoring.cpp
+++ Viewer/Viewer/Server/GetMonitoring.cpp
@@ -64,19 +64,19 @@ void GetMonitorCapture(SOCKET s)
 		if (alg_res == SUCCESS_ALGR)
 		{
 			if (GDI_ERROR == StretchDIBits(upArgh.dc_WindowPaint,
-											0,
-											0,
-											upArgh.width_main_dc,
-											upArgh.height_main_dc,
-											0,
-											0,
-											upArgh.width_sourse_cupture,
-											upArgh.height_sourse_cupture,
-											Packet_bits_cupture.data(),
-											&upArgh.bi,
-											DIB_RGB_COLORS,
-											SRCCOPY))
-				std::cout << "StrtchDibBits = 0" << GetLastError() << std::endl;
+						        0,
+							0,
+							upArgh.width_main_dc,
+							upArgh.height_main_dc,
+							0,
+							0,
+							upArgh.width_sourse_cupture,
+							upArgh.height_sourse_cupture,
+							Packet_bits_cupture.data(),
+							&upArgh.bi,
+							DIB_RGB_COLORS,
+							SRCCOPY))
+			//std::cout << "StrtchDibBits = 0" << GetLastError() << std::endl;
 		}
 		Sleep(1);
 	}
